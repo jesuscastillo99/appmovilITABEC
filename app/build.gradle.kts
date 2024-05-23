b161dcdc073eb5b08fd0cc3b7e4fe02b61687d74
Leonardo Coronado <coronadoleonardo10@gmail.com>
2024-05-23 10:48:19 -0600

    Probando 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
 dependencies {
 
+    implementation ("com.airbnb.android:lottie:5.2.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/myapplicationf/presentation/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/example/myapplicationf/presentation/ui/SplashScreenActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplicationf.R
-
+import com.airbnb.lottie.LottieAnimationView
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +16,11 @@ class SplashScreenActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        // Configurar y reproducir la animación Lottie
+        val lottieAnimationView: LottieAnimationView = findViewById(R.id.lottieAnimationView)
+        lottieAnimationView.setAnimation("Loading.json") // Si el archivo está en assets
+        lottieAnimationView.playAnimation()
     }
 }
